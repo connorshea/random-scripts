79f14ca4ba2b1cc0844bfbe12094d401408574e7
Connor Shea <connor.james.shea@gmail.com>
2019-02-03 12:48:04 -0700

    More work on the wikidata helper

--- wikidata/wikidata_helper.rb
+++ wikidata/wikidata_helper.rb
@@ -3,44 +3,27 @@ module WikidataHelper
   require "open-uri"
   require "json"
 
-  def api_test
-    query_options = [:format, :props, :languages, :ids]
-    query_options_string = query_options.join(',')
-
-    template = Addressable::Template.new(
-      "https://{host}{/segments*}{?#{query_options_string}}"
-    )
-
-    uri = Addressable::URI.parse(
-      "https://www.wikidata.org/w/api.php?format=json"
-    )
-
-    puts uri.inspect
-    puts template.inspect
-
-    parsed_uri = template.extract(uri)
-
-    puts parsed_uri.inspect
-  end
-
   #
   # Make an API call.
   #
   # @param [String] action The action to perform, see https://www.wikidata.org/w/api.php?action=help&modules=main
   # @param [String] ids Wikidata IDs, e.g. 'Q123' or 'P123'
   # @param [String] props Property type
-  # @param [<Type>] languages <description>
+  # @param [String] languages Language code
+  # @param [String] entity Wikidata entity ID, e.g. 'Q123'
   #
-  # @return [<Type>] <description>
+  # @return [Hash] Ruby hash form of the Wikidata JSON Response.
   #
-  def api(action: nil, ids: nil, props: nil, languages: 'en')
+  def api(action: nil, ids: nil, props: nil, languages: 'en', entity: nil, property: nil)
     query_options = [
       :format,
       :action,
       :props,
       :languages,
       :ids,
-      :sitelinks
+      :sitelinks,
+      :entity,
+      :property
     ]
     query_options_string = query_options.join(',')
     
@@ -50,16 +33,20 @@ module WikidataHelper
       'format': 'json',
       'ids': ids,
       'languages': languages,
-      'props': props
+      'props': props,
+      'entity': entity,
+      'property': property
     })
 
-    puts template
+    puts template if ENV['DEBUG']
     api_uri = URI.parse(template.to_s)
 
     response = JSON.load(open(api_uri))
 
-    if response['success']
+    if response['success'] && action == 'wbgetentities'
       return response['entities']["#{ids}"]
+    elsif action == 'wbgetclaims'
+      return response['claims']
     else
       return nil
     end
@@ -70,14 +57,8 @@ module WikidataHelper
       action: 'wbgetentities',
       ids: ids
     )
-  end
 
-  def get_claims(ids:)
-    response = api(
-      action: 'wbgetentities',
-      ids: ids,
-      props: 'claims'
-    )
+    return response
   end
 
   def get_descriptions(ids:)
@@ -86,6 +67,8 @@ module WikidataHelper
       ids: ids,
       props: 'descriptions'
     )
+
+    return response
   end
 
   def get_datatype(ids:)
@@ -94,6 +77,8 @@ module WikidataHelper
       ids: ids,
       props: 'datatype'
     )
+
+    return response
   end
 
   def get_aliases(ids:)
@@ -102,18 +87,34 @@ module WikidataHelper
       ids: ids,
       props: 'aliases'
     )
+
+    return response
   end
 
-  def get_labels(ids:)
+  #
+  # Get labels ('names') of the given items
+  #
+  # @param [String] ids <description>
+  # @param [String, Array<String>] languages A country code or array of country codes, e.g. 'en' or ['en', 'es']
+  #
+  # @return [Hash] Hash of labels in the listed languages.
+  #
+  def get_labels(ids:, languages: nil)
+    if languages.is_a?(Array)
+      languages = languages.join('|')
+    end
+
     response = api(
       action: 'wbgetentities',
       ids: ids,
       props: 'labels',
-      languages: nil
+      languages: languages
     )
 
-    puts response.inspect
+    return response['labels']
   end
+
+  alias_method :get_names, :get_labels
 
   #
   # Get sitelinks for a given Wikidata item.
@@ -121,6 +122,19 @@ module WikidataHelper
   # @param [String] ids Wikidata IDs, e.g. 'Q123' or 'P123'
   #
   # @return [Hash] Returns a hash of sitelinks.
+  #
+  # {
+  #   "afwiki"=>{
+  #     "site"=>"afwiki",
+  #     "title"=>"Douglas Adams",
+  #     "badges"=>[]
+  #   },
+  #   "bswiki"=>{
+  #     "site"=>"bswiki",
+  #     "title"=>"Douglas Adams",
+  #     "badges"=>[]
+  #   }
+  # }
   #
   def get_sitelinks(ids:)
     response = api(
@@ -134,13 +148,80 @@ module WikidataHelper
 
     return sitelinks
   end
+
+#
+# Get claims about an Wikidata entity.
+#
+# @param [string] entity Wikidata entity ID, e.g. 'Q123'
+# @param [string] property Wikidata property ID, e.g. 'P123'
+#
+# @return [Hash] Returns a hash with the properties of the entity.
+#
+def get_claims(entity:, property: nil)
+    response = api(
+      action: 'wbgetclaims',
+      entity: entity,
+      languages: nil,
+      property: property
+    )
+
+    return response
+  end
 end
 
 include WikidataHelper
 
-WikidataHelper.get_claims(ids: 'Q42')
-WikidataHelper.get_descriptions(ids: 'Q42')
-WikidataHelper.get_datatype(ids: 'P42')
-WikidataHelper.get_aliases(ids: 'Q42')
-WikidataHelper.get_labels(ids: 'Q42')
-WikidataHelper.get_sitelinks(ids: 'Q42')
+def prettify(input)
+  return JSON.pretty_generate(input)
+end
+
+def claims_helper(game, property)
+  return WikidataHelper.get_claims(entity: game, property: PROPERTIES[property])
+end
+
+def get_english_name(id)
+  names = WikidataHelper.get_names(ids: id, languages: 'en')
+  return names['en']['value']
+end
+
+# WikidataHelper.get_claims(entity: 'Q4200', property: 'P31')
+# WikidataHelper.get_descriptions(ids: 'Q42')
+# WikidataHelper.get_datatype(ids: 'P42')
+# WikidataHelper.get_aliases(ids: 'Q42')
+# WikidataHelper.get_labels(ids: 'Q42')
+# WikidataHelper.get_sitelinks(ids: 'Q42')
+
+PROPERTIES = {
+  publishers: 'P123',
+  platforms: 'P400',
+  developers: 'P178',
+  genres: 'P136',
+  publication_dates: 'P577'
+}
+
+games = {
+  'Half-Life': 'Q279744',
+  'Call of Duty 4: Modern Warfare': 'Q76255',
+  'Half-Life 2': 'Q193581',
+  'Half-Life 2: Episode One': 'Q18951',
+  'Half-Life 2: Episode Two': 'Q553308',
+  'Portal 2': 'Q279446',
+  "Kirby's Epic Yarn": 'Q1361363',
+  'Doom': 'Q513867'
+}
+
+half_life = {}
+
+half_life['name'] = get_english_name(games['Half-Life'.to_sym])
+
+half_life_genres = claims_helper(games['Half-Life'.to_sym], :genres)
+puts prettify(half_life_genres)
+
+half_life['genres'] = half_life_genres[PROPERTIES[:genres]]
+
+half_life_developers = claims_helper(games['Half-Life'.to_sym], :developers)
+puts prettify(half_life_developers)
+
+half_life['developers'] = half_life_developers[PROPERTIES[:developers]]
+
+puts prettify(half_life)
