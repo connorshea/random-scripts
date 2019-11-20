bfe8d2d680d552eb0cec941a094ec2e5ed994916
Connor Shea <connor.james.shea@gmail.com>
2019-11-19 23:41:05 -0700

    Update pcgw_purge.rb

--- wikidata/pcgw-export/pcgw_purge.rb
+++ wikidata/pcgw-export/pcgw_purge.rb
@@ -20,7 +20,7 @@ def get_all_humble_store_pages(limit: 25, offset: 0)
     all_pages << result
   end
   if pages.key?('query-continue-offset')
-    all_pages.concat(get_all_humble_store_pages(offset: pages['query-continue-offset'].to_i))
+    all_pages.concat(get_all_humble_store_pages(limit: limit, offset: pages['query-continue-offset'].to_i))
   end
   return all_pages
 end
@@ -55,17 +55,42 @@ client = MediawikiApi::Client.new "https://www.pcgamingwiki.com/w/api.php"
 
 client.log_in ENV["PCGW_USERNAME"], ENV["PCGW_PASSWORD"]
 
-humble_pages = get_all_humble_store_pages(limit: 300)
+# humble_pages = get_all_humble_store_pages(limit: 300)
 
-puts humble_pages.inspect
+# Was having trouble getting the data programmatically so we're doing this from a local JSON file :)
+humble_store_json_file = File.join(File.dirname(__FILE__), 'humble-store-ids.json')
+humble_pages = JSON.load(File.read(humble_store_json_file))
 
-batch_count = humble_pages.count % 40
-batch_count.times do |batch_num|
-  puts "Purging 40 PCGW articles."
-  current_batch = pcgw_ids[(40 * batch_num)..(40 * (batch_num + 1))]
-  puts current_batch.join('|')
-  client.action :purge, titles: current_batch.join('|')
-  puts "Purge done, sleeping..."
-  sleep 10
+humble_pages = humble_pages['query']['results']
+humble_pages = humble_pages.map { |page| page[1]["fullurl"].sub('https://www.pcgamingwiki.com/wiki/', '') }
+
+# puts humble_pages.inspect
+
+puts "#{humble_pages.count} pages to purge"
+humble_pages.each_with_index do |page, index|
+  puts "Purging #{page}"
+  response = client.action :purge, titles: URI.unescape(page), formatversion: 2
+  puts response.data.first['purged']
+  puts response.warnings.inspect
+  sleep 2
+  begin
+    URI.open("https://www.pcgamingwiki.com/wiki/#{page}")
+  rescue OpenURI::HTTPError => e
+    puts e
+  end
+  puts index
 end
+
+#   batch_count.times do |batch_num|
+#   next if batch_num < 6
+#   puts "Purging 40 PCGW articles."
+#   current_batch = humble_pages[(40 * batch_num)..(40 * (batch_num + 1))]
+#   titles = current_batch.map { |x| URI.unescape(x) }.join('|')
+#   puts titles
+#   response = client.action :purge, titles: titles, formatversion: 2
+#   puts response.inspect
+#   puts response.warnings.inspect
+#   puts "Purge done, sleeping..."
+#   sleep 30
+# end
 
