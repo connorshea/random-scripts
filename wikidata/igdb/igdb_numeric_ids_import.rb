65b694bd70a8bec289b38b1d6123ebe1954015ef
Connor Shea <connor.james.shea@gmail.com>
2021-01-22 21:45:52 -0700

    Complete script to add IGDB numeric IDs as qualifiers
    Based partially on the script for importing platform qualifiers on
    Steam IDs.
    
    Some games have more than one IGDB ID, so this makes an effort
    to handle that edge case properly.

--- wikidata/igdb/igdb_numeric_ids_import.rb
+++ wikidata/igdb/igdb_numeric_ids_import.rb
@@ -35,18 +35,16 @@ trap("SIGINT") { exit! }
 IGDB_GAME_PID = 'P5794'
 IGDB_NUMERIC_GAME_PID = 'P9043'
 
-# TODO: Write a query for finding all IGDB IDs without the numeric qualifier.
+# Query for finding all IGDB IDs without the numeric qualifier.
 def query
   return <<-SPARQL
-    SELECT ?item ?itemLabel ?malId WHERE
-    {
-      VALUES ?animeTypes {
-        wd:Q20650540 # anime film
+    SELECT ?item ?itemLabel ?igdbId ?numericId WHERE {
+      OPTIONAL {
+        ?item p:#{IGDB_GAME_PID} ?statement. # Get the statement of the IGDB ID
+        ?statement ps:#{IGDB_GAME_PID} ?igdbId. # Get the actual ID
+        FILTER(NOT EXISTS { ?statement pq:#{IGDB_NUMERIC_GAME_PID} ?numericId. }) # Get rid of anything with a numeric IGDB ID qualifier.
       }
-      ?item wdt:P31 ?animeTypes;
-            wdt:#{MAL_ANIME_PID} ?malId. # with a MAL ID
-      FILTER NOT EXISTS { ?item wdt:#{ANILIST_ANIME_PID} ?anilistId . } # and no AniList ID
-      SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "en,en". }
     }
   SPARQL
 end
@@ -85,8 +83,61 @@ wikidata_rows.each do |row|
   key_hash = row.to_h
   wikidata_item = {
     name: key_hash[:itemLabel].to_s,
-    wikidata_id: key_hash[:item].to_s.sub('http://www.wikidata.org/entity/Q', '')
+    wikidata_id: key_hash[:item].to_s.sub('http://www.wikidata.org/entity/Q', ''),
+    igdb_id: key_hash[:igdbId].to_s
   }
 
   wikidata_items << wikidata_item
+end
+
+progress_bar = ProgressBar.create(
+  total: wikidata_items.count,
+  format: "\e[0;32m%c/%C |%b>%i| %e\e[0m"
+)
+
+wikidata_items.each do |wikidata_item|
+  igdb_id = wikidata_item[:igdb_id]
+
+  # Find the numeric IGDB ID associated with the Wikidata item's IGDB ID.
+  igdb_game = igdb_games.find { |igdb_game| igdb_game[:slug] == igdb_id }
+  
+  if igdb_game.nil?
+    progress_bar.log 'No IGDB numeric ID could be derived from the IGDB ID.'
+    progress_bar.increment
+    next
+  end
+
+  igdb_numeric_id = igdb_game[:id]
+
+  igdb_claims = WikidataHelper.get_claims(entity: "Q#{wikidata_item[:wikidata_id]}", property: IGDB_GAME_PID).dig(IGDB_GAME_PID)
+  # Simplify the claims just to the parts we care about: the IGDB slug and the claim ID.
+  igdb_claims.map! do |claim|
+    {
+      claim_id: claim.dig('id'),
+      igdb_slug: claim.dig('mainsnak', 'datavalue', 'value')
+    }
+  end
+
+  # Filter to the claim specifically about the given IGDB slug, in case
+  # there are multiple on the same game item.
+  igdb_claim_id = igdb_claims.find { |claim| claim[:igdb_slug] == igdb_game[:slug] }&.dig(:claim_id)
+
+  if igdb_claim_id.nil?
+    progress_bar.log 'No relevant claim ID could be found.'
+    progress_bar.increment
+    next
+  end
+
+  sleep 1
+  progress_bar.log "--------------------------"
+  progress_bar.log "Wikidata ID: Q#{wikidata_item[:wikidata_id]}"
+  progress_bar.log "IGDB Numeric ID: #{igdb_numeric_id}"
+  progress_bar.log "IGDB Slug: #{igdb_game[:slug]}"
+
+  begin
+    wikidata_client.set_qualifier(igdb_claim_id.to_s, 'value', IGDB_NUMERIC_GAME_PID, igdb_numeric_id.to_s.to_json)
+  rescue => error
+    progress_bar.log "ERROR: #{error}"
+  end
+  progress_bar.increment
 end
