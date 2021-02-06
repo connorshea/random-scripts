2a819f572667fef2ccfe8fbb351ba43c9b9bd5af
Connor Shea <connor.james.shea@gmail.com>
2021-02-06 12:22:03 -0700

    Add reference url, stated in, and retreived to StrategyWiki statement.
    This sucked to put together because the Wikidata API has barely any docs
    and figuring out what to input for these values was a hellish nightmare.
    
    But it works now, yay.

--- wikidata/pcgw/export/pcgw_add_strategywiki_to_wikidata.rb
+++ wikidata/pcgw/export/pcgw_add_strategywiki_to_wikidata.rb
@@ -56,6 +56,67 @@ progress_bar = ProgressBar.create(
   format: "\e[0;32m%c/%C |%b>%i| %e\e[0m"
 )
 
+REFERENCE_PROPERTIES = {
+  stated_in: 'P248',
+  retreived: 'P813',
+  reference_url: 'P854'
+}
+
+PCGAMINGWIKI_PID = 17013880
+
+# Need to use wbsetreference API here. The JSON snak input for adding the
+# reference URL, stated in, and retreived values looks like this, it is
+# horrendous:
+#
+# ```json
+# {
+#   "P248": [
+#     {
+#       "snaktype": "value",
+#       "property": "P248",
+#       "datatype": "wikibase-item",
+#       "datavalue": {
+#         "value": {
+#           "entity-type": "item",
+#           "numeric-id": 17013880,
+#           "id": "Q17013880"
+#         },
+#         "type": "wikibase-entityid"
+#       }
+#     }
+#   ],
+#   "P813": [
+#     {
+#       "snaktype": "value",
+#       "property": "P813",
+#       "datatype": "time",
+#       "datavalue": {
+#         "value": {
+#           "time": "+2021-02-06T00:00:00Z",
+#           "timezone": 0,
+#           "before": 0,
+#           "after": 0,
+#           "precision": 11,
+#           "calendarmodel": "http://www.wikidata.org/entity/Q1985727"
+#         },
+#         "type": "time"
+#       }
+#     }
+#   ],
+#   "P854": [
+#     {
+#       "snaktype": "value",
+#       "property": "P854",
+#       "datatype": "url",
+#       "datavalue": {
+#         "value": "https://www.pcgamingwiki.com/wiki/De_Blob",
+#         "type": "string"
+#       }
+#     }
+#   ]
+# }
+# ```
+
 rows.each do |row|
   progress_bar.increment
 
@@ -93,7 +154,66 @@ rows.each do |row|
     claim = wikidata_client.create_claim(wikidata_id, "value", "P9075", "\"#{strategy_wiki_id}\"")
   rescue MediawikiApi::ApiError => e
     progress_bar.log e
+    next
   end
+
+  # Get the claim ID returned from the create_claim method.
+  claim_id = claim.data.dig('claim', 'id')
+
+  snak = {
+    REFERENCE_PROPERTIES[:stated_in] => [
+      {
+        "snaktype" => "value",
+        "property" => REFERENCE_PROPERTIES[:stated_in],
+        "datatype" => "wikibase-item",
+        "datavalue" => {
+          "value" => {
+            "entity-type" => "item",
+            "numeric-id" => PCGAMINGWIKI_PID,
+            "id" => "Q#{PCGAMINGWIKI_PID}"
+          },
+          "type" => "wikibase-entityid"
+        }
+      }
+    ],
+    REFERENCE_PROPERTIES[:retreived] => [
+      {
+        "snaktype" => "value",
+        "property" => REFERENCE_PROPERTIES[:retreived],
+        "datatype" => "time",
+        "datavalue" => {
+          "value" => {
+            "time" => Date.today.strftime("+%Y-%m-%dT%H:%M:%SZ"),
+            "timezone" => 0,
+            "before" => 0,
+            "after" => 0,
+            "precision" => 11,
+            "calendarmodel" => "http://www.wikidata.org/entity/Q1985727"
+          },
+          "type" => "time"
+        }
+      }
+    ],
+    REFERENCE_PROPERTIES[:reference_url] => [
+      {
+        "snaktype" => "value",
+        "property" => REFERENCE_PROPERTIES[:reference_url],
+        "datatype" => "url",
+        "datavalue" => {
+          "value" => "https://www.pcgamingwiki.com/wiki/#{key_hash[:pcgw_id].to_s}",
+          "type" => "string"
+        }
+      }
+    ]
+  }
+
+  progress_bar.log 'Adding reference to statement...'
+  begin
+    wikidata_client.set_reference(claim_id, snak.to_json)
+  rescue MediawikiApi::ApiError => e
+    progress_bar.log e
+  end
+  sleep 1
 end
 
 progress_bar.finish unless progress_bar.finished?
