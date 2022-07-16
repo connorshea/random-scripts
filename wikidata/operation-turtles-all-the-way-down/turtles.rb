31d8444500f229fb62c5dc24a7a61e78cc90958e
Connor Shea <connor.james.shea@gmail.com>
2022-07-16 10:46:16 -0600

    Refactor turtles.rb a bit.
    And write out the process it'll follow.

--- wikidata/operation-turtles-all-the-way-down/turtles.rb
+++ wikidata/operation-turtles-all-the-way-down/turtles.rb
@@ -23,76 +23,116 @@
 # - https://www.wikidata.org/wiki/Q1191210
 # - https://www.wikidata.org/wiki/Q531723
 
+##
+# Goal of this script:
+# - Grab all the items on Wikidata that are both an anime and a manga.
+# - Go through each item one at a time.
+#   - Using the lists of properties that are specific to one type (or are relevant to both), split the statements on the item into their own independent manga and anime items.
+#   - Create the manga item.
+#   - Create the anime item.
+#   - Remove the properties that are no longer relevant from the original, shared item.
+#   - Change "instance of" on the original shared item to "media franchise".
+#   - Manually fix any issues on the three items as-necessary.
+#     - For example, publication date.
+#   - Continue to the next item.
+
 # SPARL Query for getting all items on Wikidata that are both a "manga series"
-# and "anime television series". This returns around 800 items. We'll need a
-# second query to also get those listed as both a manga and an "anime film".
+# and "anime television series", but not a light novel or video game.
+# This returns around 700 items. We might want to modify this to include
+# items marked as "anime film" as well.
 SPARQL_QUERY = <<~SPARQL
   SELECT DISTINCT ?item ?itemLabel WHERE {
     SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
     {
       SELECT DISTINCT ?item WHERE {
+        # Items that are both an anime television series and a manga series.
         ?item p:P31 ?statement0.
-        ?statement0 (ps:P31/(wdt:P279*)) wd:Q21198342.
+        ?statement0 ps:P31 wd:Q21198342.
         ?item p:P31 ?statement1.
-        ?statement1 (ps:P31/(wdt:P279*)) wd:Q63952888.
+        ?statement1 ps:P31 wd:Q63952888.
+        # Exclude those that are also marked as light novels, to reduce how
+        # many complex cases we need to handle.
+        MINUS {
+          ?item p:P31 ?statement2.
+          ?statement2 (ps:P31/(wdt:P279*)) wd:Q747381.
+        }
+        # Exclude light novel series'
+        MINUS {
+          ?item p:P31 ?statement3.
+          ?statement3 (ps:P31/(wdt:P279*)) wd:Q104213567.
+        }
+        # Exclude video games
+        MINUS {
+          ?item p:P31 ?statement4.
+          ?statement4 (ps:P31/(wdt:P279*)) wd:Q7889.
+        }
       }
     }
   }
 SPARQL
 
-ANIME_SPECIFIC_PROPERTIES = [
-  'P1811', # list of episodes
-  'P1113', # number of episodes
-  'P2437', # number of seasons
-  'P364', # original language of film or TV show
-  'P449', # original broadcaster
-  'P2047', # duration
-  'P86', # composer
-  'P57', # director
-  'P725', # voice actor
+ANIME_TELEVISION_SERIES_QID = 'Q63952888'.freeze
+MANGA_SERIES_QID = 'Q21198342'.freeze
+LIGHT_NOVEL_QID = 'Q747381'.freeze
+LIGHT_NOVEL_SERIES_QID = 'Q104213567'.freeze
+VIDEO_GAME_QID = 'Q7889'.freeze
 
-  'P4086', # MyAnimeList anime ID
-  'P8729', # AniList anime ID
-  'P1985', # Anime News Network anime ID
-  'P5845', # AnimeClick anime ID
-  'P1874', # Netflix ID
-  'P1267', # AlloCiné series ID
-  'P4110', # Crunchyroll ID
-  'P4983', # TMDb TV series ID
-  'P3138', # OFDb film ID
-  'P2603', # Kinopoisk film ID
-  'P6992', # IMFDB ID
-  'P345', # IMDb ID
-  'P480', # FilmAffinity ID
-  'P5646', # AniDB anime ID
-  'P2603', # Kinopoisk film ID
-  'P3138', # OFDb film ID
-  'P4835', # TheTVDB.com series ID
-  'P5327', # fernsehserien.de ID
-  'P5032', # Filmweb.pl film ID
-  'P7107', # LezWatch.TV show ID
-  'P4834', # Deutsche Synchronkartei series ID
-  'P5387', # Behind The Voice Actors TV show ID
-  'P4529', # Douban film ID
-  'P6127', # Letterboxd film ID
-  'P4947', # TMDb movie ID
-  'P5925', # Moviepilot.de series ID
-].freeze
+ANIME_SPECIFIC_PROPERTIES = {
+  'list of episodes': 'P1811',
+  'number of episodes': 'P1113',
+  'number of seasons': 'P2437',
+  'original language of film or TV show': 'P364',
+  'original broadcaster': 'P449',
+  'duration': 'P2047',
+  'composer': 'P86',
+  'director': 'P57',
+  'voice actor': 'P725',
+  'discography': 'P358',
 
-MANGA_SPECIFIC_PROPERTIES = [
-  'P4087', # MyAnimeList manga ID
-  'P8731', # AniList manga ID
-  'P1984', # Anime News Network manga ID
-  'P5849', # AnimeClick manga ID
-  'P6947', # Goodreads series ID
-].freeze
+  'MyAnimeList anime ID': 'P4086',
+  'AniList anime ID': 'P8729',
+  'Anime News Network anime ID': 'P1985',
+  'AnimeClick anime ID': 'P5845',
+  'Netflix ID': 'P1874',
+  'AlloCiné series ID': 'P1267',
+  'Crunchyroll ID': 'P4110',
+  'TMDb TV series ID': 'P4983',
+  'OFDb film ID': 'P3138',
+  'Kinopoisk film ID': 'P2603',
+  'IMFDB ID': 'P6992',
+  'IMDb ID': 'P345',
+  'FilmAffinity ID': 'P480',
+  'AniDB anime ID': 'P5646',
+  'Kinopoisk film ID': 'P2603',
+  'OFDb film ID': 'P3138',
+  'TheTVDB.com series ID': 'P4835',
+  'fernsehserien.de ID': 'P5327',
+  'Filmweb.pl film ID': 'P5032',
+  'LezWatch.TV show ID': 'P7107',
+  'Deutsche Synchronkartei series ID': 'P4834',
+  'Behind The Voice Actors TV show ID': 'P5387',
+  'Douban film ID': 'P4529',
+  'Letterboxd film ID': 'P6127',
+  'TMDb movie ID': 'P4947',
+  'Moviepilot.de series ID': 'P5925'
+}.freeze
 
-SHARED_PROPERTIES = [
-  'P407', # language of work or name
-  'P495', # country of origin
-  'P8345', # media franchise
-  'P136', # genre
-  'P2360', # intended public
-  'P921', # main subject
-  'P1881', # list of characters
-].freeze
+MANGA_SPECIFIC_PROPERTIES = {
+  'MyAnimeList manga ID': 'P4087',
+  'AniList manga ID': 'P8731',
+  'Anime News Network manga ID': 'P1984',
+  'AnimeClick manga ID': 'P5849',
+  'Goodreads series ID': 'P6947',
+}.freeze
+
+SHARED_PROPERTIES = {
+  'language of work or name': 'P407',
+  'country of origin': 'P495',
+  'media franchise': 'P8345',
+  'genre': 'P136',
+  # NOTE: Anime items may have a constraint violation if this property is
+  # used? Not sure why it's set up that way, but yeah.
+  'intended public': 'P2360',
+  'main subject': 'P921',
+  'list of characters': 'P1881',
+}.freeze
